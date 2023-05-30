88e0de4c9531f00686a07f45943a25a540beee5c
openthc <code@openthc.com>
2023-05-30 22:25:27 +0000

    exmaple config

---
+++ etc/config-example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Data Configuration Example
+ */
+
+$cfg = [];
+
+$cfg['database'] = [
+	'data' => [
+		'hostname' => '10.3.10.1',
+		'database' => 'openthc',
+		'username' => 'openthc',
+		'password' => '01DD95AP0HMQ312FGJAGHQ3QS7',
+	]
+];
+
+return $cfg;
