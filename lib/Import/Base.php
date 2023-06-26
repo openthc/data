3456c17340533985f86c13c2867838d51b4a268e
openthc <code@openthc.com>
2023-06-26 14:44:57 +0000

    Use Base import class

---
+++ lib/Import/Base.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Import Base Class
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+namespace OpenTHC\Data\Import;
+
+class Base
+{
+	protected $_object;
+
+	protected $_output_path;
+
+	protected $_source_type;
+
+	protected $_source_file;
+
+	/**
+	 *
+	 */
+	function __construct(array $cfg)
+	{
+		$this->_object = strtoupper($cfg['object']);
+		$obj_list = [
+			'SECTION', 'VARIETY', 'PRODUCT',
+			'CROP', 'PLANT',
+			'INVENTORY', 'INVENTORY-ADJUST',
+			'B2B-INCOMING', 'B2B-OUTGOING',
+		];
+		if ( ! in_array($this->_object, $obj_list)) {
+			throw new \Exception('Invalid Object [LIB-033]');
+		}
+
+		$this->_source_type = $cfg['source']['type'];
+		$this->_source_file = $cfg['source']['file'];
+		if ( ! is_file($this->_source_file)) {
+			throw new \Exception('Invalid Source File [LIB-039]');
+		}
+
+		$this->_output_path = $cfg['output'];
+		if ( ! is_dir($this->_output_path)) {
+			throw new \Exception('Invalid Output Path [LIB-044]');
+		}
+
+	}
+
+}
--- lib/Import/GrowFlow.php
+++ lib/Import/GrowFlow.php
@@ -7,32 +7,8 @@
 
 namespace OpenTHC\Data\Import;
 
-class GrowFlow
+class GrowFlow extends Base
 {
-	/**
-	 *
-	 */
-	function __construct(array $cfg)
-	{
-		$this->_object = strtoupper($cfg['object']);
-		$obj_list = [
-			'SECTION', 'VARIETY', 'PRODUCT',
-			'CROP', 'PLANT',
-			'INVENTORY', 'INVENTORY-ADJUST',
-			'B2B-INCOMING', 'B2B-OUTGOING',
-		];
-		if ( ! in_array($this->_object, $obj_list)) {
-			throw new \Exception('Invalid Object [LIG-025]');
-		}
-
-		$this->_source_type = $cfg['source']['type'];
-		$this->_source_file = $cfg['source']['file'];
-		if ( ! is_file($this->_source_file)) {
-			throw new \Exception('Invalid Source File [LIG-020]');
-		}
-
-	}
-
 	/**
 	 *
 	 */
