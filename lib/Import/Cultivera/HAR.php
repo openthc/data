f441091caf46df0549eb496c5ed83f7b12c7edb2
openthc <code@openthc.com>
2023-11-16 18:35:50 +0000

    import Culitvera HAR files

---
+++ lib/Import/Cultivera/HAR.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Cultivera HAR Reader
+ *
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+namespace OpenTHC\Data\Import\Cultivera;
+
+class HAR extends \OpenTHC\Data\Import\Base
+{
+	/**
+	 * Ouptut the OpenTHC Style Data Files
+	*/
+	function output()
+	{
+		$output_idx = 1;
+
+		$res = array();
+		$data = file_get_contents($this->_source_file);
+		$data = json_decode($data , true);
+
+		// log.version; log.creator; log.pages; log.entries
+		// foreach ($data['log']['pages'] as $e) {
+		// 	$x = json_encode($e);
+		// 	if (preg_match('/20046038218353349/', $x)) {
+		// 		// echo "OG";
+		// 		// var_dump($e);
+		// 		// exit;
+		// 	}
+		// }
+
+		foreach ($data['log']['entries'] as $e) {
+
+			// $key_list = [ '_initiator', '_priority', '_resourceType', 'cache', 'connection', 'serverIPAddress', 'startedDateTime', 'timings', 'time' ];
+			// foreach ($key_list as $k) {
+			// 	unset($e[$k]);
+			// }
+
+			$e['request']['url'] = strtolower($e['request']['url']);
+
+			// $x = json_encode($e);
+			// if (preg_match('/20046038218353349/', $x)) {
+			// 	echo "ENT";
+			// 	var_dump($e);
+			// 	exit;
+			// }
+
+			$vp = sprintf('%s/%s', $e['request']['method'], $e['request']['url']);
+			echo "REQ: $vp = {$e['response']['content']['mimeType']}\n";
+			if ('application/json' == $e['response']['content']['mimeType']) {
+				if ( ! empty($e['response']['content']['text'])) {
+
+					$e['has_json'] = true;
+
+					// $path = parse_url($e['request']['url'], PHP_URL_PATH);
+					// $path = trim($path, '/');
+					// $path = rawurlencode($path);
+
+					// $f = sprintf('%s/%s-%s.json', $this->_output_path, $e['startedDateTime'], $path);
+					// file_put_contents($f, $e['response']['content']['text']);
+
+				}
+			}
+
+			switch ($vp) {
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/all-inventory-rooms':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/all-plant-rooms?name=':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/all-rooms/':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/get-roomsummary':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/get-roomtypes':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/get-subroom-types/':
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/facility/producer-inventory-rooms':
+
+					if ($e['has_json']) {
+						$res = json_decode($e['response']['content']['text'], true);
+						$this->output_section($res);
+					}
+
+					break;
+
+				case 'GET/https://api-wa.cultiverapro.com/api/v1/product/all-strains':
+
+					if ($e['has_json']) {
+						$res_src = json_decode($e['response']['content']['text'], true);
+						$this->output_variety($res_src);
+					}
+
+					break;
+
+				case 'POST/https://api-wa.cultiverapro.com/api/v1/product/get-product-batches/':
+
+					if ($e['has_json']) {
+						$res = json_decode($e['response']['content']['text'], true);
+						$this->output_inventory($res['Data']);
+					}
+
+					break;
+
+				case 'POST/https://api-wa.cultiverapro.com/api/v1/product/products':
+
+					if ($e['has_json']) {
+						$res = json_decode($e['response']['content']['text'], true);
+						$this->output_product($res['Data']);
+					}
+
+					break;
+			}
+
+			// case 'https://api-wa.cultiverapro.com/api/v1/grow/get-grow-cycles':
+			// case 'https://api-wa.cultiverapro.com/api/v1/plants/plants':
+
+		}
+
+	}
+
+	/**
+	 *
+	 */
+	function output_inventory($res)
+	{
+		foreach ($res as $src) {
+
+			$out = [];
+			$out['id'] = $src['Barcode'];
+			$out['section'] = [
+				'id' => $src['RoomId'],
+				'name' => $src['Room'],
+			];
+			$out['variety'] = [];
+			$out['product'] = [
+				'name' => $src['ProductName'],
+				'type' => [
+					'id' => '',
+					'name' => $src['InventoryTypeName'],
+				]
+			];
+			$out['qty'] = $src['RemainingQuantity'];
+			// $out[''] = $src[''];
+
+			// Location == License Code
+			// InventoryTypeCode == BioTrack Type Code
+			// Inventory Type Name
+
+			// HasQaResult
+			// Thc
+			// Cbd
+			// Thca
+			// Total
+			// BatchDate
+
+			$f = sprintf('%s/inventory-%s.json', $this->_output_path, $out['id']);
+			file_put_contents($f, json_encode($out, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+		}
+
+	}
+
+	/**
+	 * Fold to OpenTHC Data and Output
+	 */
+	function output_product($res)
+	{
+		// $res_src = array_change_key_case($res_src);
+		echo "PRODUCT: " . count($res) . "\n";
+		// $d = json_encode($res_src);
+		// $f = sprintf('%s/product-dump.json', $this->_output_path);
+		// file_put_contents($f, $d);
+		foreach ($res as $src) {
+			// var1_dump($src);
+			// return;
+		}
+
+	}
+
+	/**
+	 * Fold to OpenTHC Data and Output
+	 */
+	function output_section($res)
+	{
+		foreach ($res as $src) {
+
+		}
+	}
+
+	/**
+	 * Fold to OpenTHC Data and Output
+	 */
+	function output_variety($res)
+	{
+		// $res_src = array_change_key_case($res_src);
+		// var_dump($res_src);
+		echo "VARITETY: " . count($res) . "\n";
+		// $d = json_encode($res_src);
+		// $f = sprintf('%s/variety-dump.json', $this->_output_path);
+		// file_put_contents($f, $d);
+
+	}
+
+}
