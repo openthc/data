b11d858285a34dff939cfa6740de9c56bbb6eebf
openthc <code@openthc.com>
2025-03-29 19:52:56 +0000

    rename & fix base sql

--- etc/sql/00-base.sql
+++ etc/sql/00-base.sql
@@ -2,20 +2,20 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
+\c openthc_data
+
 SET check_function_bodies = false;
-SET xmloption = content;
+SET client_encoding = 'UTF8';
 SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
-
 SET default_with_oids = false;
+SET idle_in_transaction_session_timeout = 0;
+SET lock_timeout = 0;
+SET row_security = off;
+SET search_path TO public;
+SET standard_conforming_strings = on;
+SET statement_timeout = 0;
+SET xmloption = content;
 
 --
 -- Name: b2b_path; Type: TABLE;
